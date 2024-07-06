e77e6eebdb9488874083008847930e5ee1b0607b
crimson206 <sisung.kim1@gmail.com>
2024-07-06 09:30:36 +0200

    version fix to 0.1.0, edit test_example

--- test/test_example.py
+++ test/test_example.py
@@ -1,16 +1,5 @@
-import unittest
+import pytest
 
-
-class TestClassName(unittest.TestCase):
-
-    def test_func_name(self):
-        expected = 3
-        a, b = 1, 2
-
-        actual = a + b
-
-        self.assertEqual(actual, expected)
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_example():
+    
+    assert 1==1
